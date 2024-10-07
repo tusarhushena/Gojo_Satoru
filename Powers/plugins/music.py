e2f8377f64fa5ba40b52cc610d6b4d028d906afa
tosu <154593488+tusarhushena@users.noreply.github.com>
2024-10-08 02:09:15 +0530

    Update music.py

--- Powers/plugins/music.py
+++ Powers/plugins/music.py
@@ -2,4 +2,13 @@ __PLUGIN__ = "Music"
 
 __alt_name__ = ["play", "vplay", "seek", "queue", "end", "skip", "seekback"]
 
-__HELP__ = 
+__HELP__ =  """
+**Music**
+
+• /play - play the song
+• /vplay - play video songs
+• /seek - seek the songs in seconds
+• /queue - list of of songs
+• /end - stop the stream
+• /skip - skip the song
+• /seekback - seek back the song in seconds."""
